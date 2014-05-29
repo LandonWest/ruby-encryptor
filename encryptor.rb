a0213158fef3270ef535ecc5aa47a3e30f155a20
LandonWest <landonwest5@gmail.com>
2014-05-29 16:30:51 -0600

    add encryptor.rb, secretmessage and encrypted message

---
+++ encryptor.rb
@@ -0,0 +1,36 @@
+class Encryptor
+
+  def cipher
+    {'A' => 'N','B' => 'O','C' => 'P','D' => 'Q','E' => 'R','F' => 'S',
+     'G' => 'T','H' => 'U','I' => 'V','J' => 'W','K' => 'X','L' => 'Y',
+     'M' => 'Z','N' => 'A','O' => 'B','P' => 'C','Q' => 'D','R' => 'E',
+     'S' => 'F','T' => 'G','U' => 'H','V' => 'I','W' => 'J','X' => 'K',
+     'Y' => 'L','Z' => 'M'}
+  end
+
+  def encrypt(letter)
+    cipher[letter.upcase]
+  end
+
+  def encrypt_word(string)
+    results = []
+    letters = string.split('')
+    letters.each do |letter|
+      encrypted_letter = encrypt(letter)
+      results << encrypted_letter
+    end
+    results.join
+  end
+
+  def encrypt_file(filename)
+    input = File.open("secret_message.txt", "r")
+    contents = input.read
+    encrypted_contents = encrypt_word(contents)
+    input.close
+
+    output = File.open("secret_message.txt" + ".encrypted", "w")
+    output.write(encrypted_contents)
+    output.close
+  end
+
+end
